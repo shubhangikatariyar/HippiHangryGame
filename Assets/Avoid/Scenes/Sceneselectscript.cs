e1097163cfebdc5970e6853f5a62ffe910f20e8b
shubhangikatariyar <55647337+shubhangikatariyar@users.noreply.github.com>
2021-07-05 01:04:04 +0530

    Adding Files

---
+++ Assets/Avoid/Scenes/Sceneselectscript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneselectscript : MonoBehaviour
+{
+    public void selectScene(){
+        switch (this.gameObject.name){
+            case "playbutton":
+            SceneManager.LoadScene ("Game Scene");
+            break; 
+        }
+    }
+}
